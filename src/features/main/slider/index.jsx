5cf2f45dad1d7eebd4ebd0504116a10dd0305118
Sergo588 <mazmanyan_1990@mail.ru>
2024-06-27 20:22:59 +0300

    update

--- src/features/main/slider/index.jsx
+++ src/features/main/slider/index.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React from 'react';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
-export const Slider = () => {
+export const SliderList = () => {
     return (
-        <div>
-            <img src="/images/main/slider/1.png"/>
+        <div className="h-[620px] relative">
+            
+            <CarouselProvider
+                naturalSlideWidth={100}
+                naturalSlideHeight={125}
+                totalSlides={3}
+            >
+                <Slider>
+                    <Slide index={0}><img src="/images/main/slider/1.png"/></Slide>
+                    <Slide index={1}><img src="/images/main/slider/1.png"/></Slide>
+                    <Slide index={2}><img src="/images/main/slider/1.png"/></Slide>
+                </Slider>
+                <ButtonBack className="absolute top-1/2 -translate-y-1/2 left-0 z-[111]">
+                    <div className="flex items-center justify-center !bg-[#000000] rounded-full w-8 h-8 z-[1111]">
+                        <img src="/icons/main/slider/arrowLeft.svg"/>
+                    </div>
+                </ButtonBack>
+                <ButtonNext className="absolute top-1/2 -translate-y-1/2 right-0 z-[111]">
+                    <div className="relative flex items-center justify-center !bg-[#000000] rounded-full w-8 h-8 z-[1111]">
+                        <img  src="/icons/main/slider/arrowRight.svg"/>
+                    </div>
+                </ButtonNext>
+            </CarouselProvider>
         </div>
     )
 }
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -3,13 +3,13 @@ import { BaseLayout } from "../../layouts/BaseLayout";
 import { Partners } from "../../features/main/partners";
 import { BrandCollection } from "../../features/main/brandСollection";
 import { Catalog } from "../../features/main/catalog";
-import { Slider } from "../../features/main/slider";
+import { Slider, SliderList } from "../../features/main/slider";
 
 export const Main = () => {
     return (
         <BaseLayout>
             <div className="flex flex-col bg-blue w-full">
-                <Slider />
+                <SliderList />
                 <Partners />
                 <Catalog />
                 <BrandCollection />
